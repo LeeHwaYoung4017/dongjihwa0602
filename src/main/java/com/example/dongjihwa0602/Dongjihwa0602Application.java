9f63259554ed09f5d1f231c1a280b4391b7478b8
LAPTOP-DI217886\LHY <ghkdud7913@naver.com>
2023-06-03 11:54:06 +0900

    Initial commit

---
+++ src/main/java/com/example/dongjihwa0602/Dongjihwa0602Application.java
@@ -0,0 +1,13 @@
+package com.example.dongjihwa0602;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dongjihwa0602Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dongjihwa0602Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dongjihwa0602/Dongjihwa0602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dongjihwa0602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dongjihwa0602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
